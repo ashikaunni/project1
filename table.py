989f3b867eb212bc02dcc1b91da2903c358e5316
ashikaunni <ashikaunni96@gmail.com>
2018-03-05 14:47:03 +0530

    second commit

---
+++ table.py
@@ -0,0 +1,6 @@
+a=input('enter limit')
+a=int(a)
+def table(a):
+	print('{0:3} {1:6} {2:9} {3:2} '.format('i','i**2','i**3','i**4'))
+	for i in range(1,a)
+		print('{0:3} {1:6} {2:9} {3:2} '.format(i,i**2,i**3,i**4))
